ca6ce6fa68451cd10a02124e0327e1c65f1f9cb3
artem-kharchenko <artem.kharchenkoo@gmail.com>
2023-04-02 23:50:18 +0300

    update ex6 log redirect within the latest changes

--- ex6_long_redirect.py
+++ ex6_long_redirect.py
@@ -1,9 +1,11 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-history_info = response.history[0]
+history_info0 = response.history[0]
+history_info1 = response.history[1]
 last_response = response
 
-print(history_info.url)
+print(history_info0.url)
+print(history_info1.url)
 print(last_response.url)
 
